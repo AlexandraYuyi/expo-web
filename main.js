9e07f07a3ae911533c3ff82b7be622c550ccc533
Gustavo López <git.gustavolopez.xyz@gmail.com>
2023-10-03 22:13:02 -0400

    fix: Arregla estilo de título

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const f = document.getElementById("foo");
-document.addEventListener(
-    "click",
-    (ev) => {
-        f.style.transform = `translateY(${ev.clientY - 25}px)`;
-        f.style.transform += `translateX(${ev.clientX - 25}px)`;
-    },
-    false,
-);
-
-function myFunction() {
-    document.getElementById("myDIV").style.transition = "all 2s";
-}
-
-
 const panels = document.querySelectorAll(".panel");
 
 const removeActiveClass = () => {

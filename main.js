21a5c07c9a5137debb1e4a90502be2f54be3e28e
andrezztop <andresguille25@outlook.com>
2023-10-05 00:57:37 -0400

    Transiciones y Animate

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ for (const panel of panels) {
         panel.classList.add("active");
     });
 }
+
+const button = document.querySelector('.button');
+const submit = document.querySelector('.submit');
+
+function toggleClass() {
+	this.classList.toggle('active');
+}
+
+function addClass() {
+	this.classList.add('finished');
+}
+
+button.addEventListener('click', toggleClass);
+button.addEventListener('transition', toggleClass);
+button.addEventListener('transition', addClass);
+
+
+function animateColor() {
+    // Get a random color
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  
+    // Set the square's background color to the random color
+    document.querySelector('.square').style.backgroundColor = randomColor;
+  }
+  
+  // Set an interval to call the animateColor function every 3 seconds
+  setInterval(animateColor, 3000);
